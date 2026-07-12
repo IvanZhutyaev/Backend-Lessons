b71a7902231ebe81071d889832c17da3560c972e
Ivan Zhutyaev <gitivanzhutyaev@gmail.com>
2026-07-12 13:14:48 +0300

    update

--- demo1/src/main/java/com/example/demo1/controller/BicycleController.java
+++ demo1/src/main/java/com/example/demo1/controller/BicycleController.java
@@ -7,5 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/bicycle")
 public class BicycleController {
-    
+    @GetMapping("/bmw")
+    String getData(){return "BMW";}
+    @GetMapping("/mersedec")
+    String getData1(){return "mersedec";}
+    @GetMapping("/kamaz")
+    String getData2(){return "KAMAZ";}
 }
